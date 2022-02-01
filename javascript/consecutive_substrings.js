08f1fbdf9b2f12a132986258296683fbfd7ee78c
Aruna <aruna.evan15@gmail.com>
2022-02-01 13:57:35 -0800

    Solution

--- javascript/consecutive_substrings.js
+++ javascript/consecutive_substrings.js
@@ -1,9 +1,15 @@
 function consecutiveSubstrings(string) {
-  // type your code here
+  if (string.length === 1){ return [string]; }
+
+  const res = [];
+  for (let i=0; i<string.length; i++){
+    res.push(string.slice(0,i+1));
+  }
+  
+  return [...res, ...consecutiveSubstrings(string.slice(1))];
 }
 
 if (require.main === module) {
-  // add your own tests in here
   console.log("Expecting: ['a', 'ab', 'abc', 'b', 'bc', 'c']");
   console.log("=>", consecutiveSubstrings('abc'));
 
@@ -11,9 +17,32 @@ if (require.main === module) {
 
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
+
+  console.log("");
+
+  console.log("Expecting: ['a', 'aa', 'a']");
+  console.log("=>", consecutiveSubstrings('aa'));
+
 }
 
 module.exports = consecutiveSubstrings;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/**
+ * 1. paraphrase
+ * 
+ * give an string >= 1 char, return array of all consecutive substrings
+ * 
+ * 2. tests
+ * 
+ * "aa"
+ * 
+ * 3. pseudocode
+ * 
+ * use recursion with returning single char as base case
+ * iterate over string, returning first, then first/second, etc.
+ * and then also return recursive call on substring string.shift()
+ * 
+ * 4. code
+ * 5. refactor
+ * 6. optimize if time
+ */
